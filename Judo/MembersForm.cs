1aacea6278796623f35851f389733b608c489740
Владимир Крупкин <krupkin.vladimir@yandex.ru>
2020-06-02 20:15:13 +0400

    Возможность добавть txt файл в DGV

--- Judo/MembersForm.cs
+++ Judo/MembersForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -72,12 +73,48 @@ namespace Judo
 
         private void ImportBut_Click(object sender, EventArgs e)
         {
+            dataGridView1.AllowUserToAddRows = false;
             OpenFileDialog OPF = new OpenFileDialog();
             OPF.Filter = "Файлы txt|*.txt|Файлы cs|*.csv";
             if (OPF.ShowDialog() == DialogResult.OK)
             {
-                MessageBox.Show(OPF.FileName);
+                string[] FileLines = File.ReadAllLines(OPF.FileName);
+                int i = 0;
+                int peri = dataGridView1.Rows.Count;
+                i += peri;
+                for (; i < FileLines.Length+peri; i++)
+                {
+                    dataGridView1.Rows.Add();
+                    string[] RowsF = FileLines[i-peri].Split(new char[] { ',', ';', ':' }, StringSplitOptions.RemoveEmptyEntries);
+                    dataGridView1.Rows[i].Cells["Имя"].Value = RowsF[0].Trim();
+                    dataGridView1.Rows[i].Cells["Фамилия"].Value = RowsF[1].Trim();
+                    dataGridView1.Rows[i].Cells["Пол"].Value = RowsF[2].Trim();
+                    dataGridView1.Rows[i].Cells["Год"].Value = RowsF[3].Trim();
+                    dataGridView1.Rows[i].Cells["Город_рождения"].Value = RowsF[4].Trim();
+                    int LastI = RowsF[RowsF.Length - 1].LastIndexOf(' ');
+                    dataGridView1.Rows[i].Cells["Вес"].Value = RowsF[RowsF.Length - 1].Substring(LastI, RowsF[RowsF.Length - 1].Length - LastI).Trim();
+                    if (RowsF.Length == 7)
+                    {
+                        dataGridView1.Rows[i].Cells["Клуб"].Value = RowsF[6].Substring(0, LastI).Trim();
+                        dataGridView1.Rows[i].Cells["Место_проживания"].Value = RowsF[5].Trim();
+                    }
+                    else dataGridView1.Rows[i].Cells["Место_проживания"].Value = RowsF[5].Substring(0, LastI).Trim();
+                    foreach (DataGridViewCell item in dataGridView1.Rows[i].Cells)
+                    {
+                        if (item.Value == null || item.Value == "")
+                        {
+                            item.Value = "Не указано";
+                        }
+
+                    }
+                }
+                dataGridView1.AllowUserToAddRows = true;
             }
+        }
+
+        private void EditBut_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
         }
     }
 }
