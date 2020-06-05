26932ad237e631eef283bb030ad251e01bb7ebca
Skrip men <kiberkot.v.roymenge@gmail.com>
2020-06-05 14:30:57 +0500

    массонский заговор блять

--- Judo/DayForm.Designer.cs
+++ Judo/DayForm.Designer.cs
@@ -32,9 +32,7 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DayForm));
             this.label1 = new System.Windows.Forms.Label();
             this.timer = new System.Windows.Forms.Timer(this.components);
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.DatePicker = new System.Windows.Forms.DateTimePicker();
             this.SuspendLayout();
             // 
             // label1
@@ -47,16 +45,6 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Введите дату проведения соревнований";
             // 
-            // textBox1
-            // 
-            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(83)))), ((int)(((byte)(86)))));
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox1.Font = new System.Drawing.Font("Arial", 14.25F);
-            this.textBox1.Location = new System.Drawing.Point(12, 55);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(356, 22);
-            this.textBox1.TabIndex = 2;
-            // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(83)))), ((int)(((byte)(86)))));
@@ -64,9 +52,9 @@
             this.button1.FlatAppearance.BorderSize = 0;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Arial", 10.25F);
-            this.button1.Location = new System.Drawing.Point(374, 55);
+            this.button1.Location = new System.Drawing.Point(374, 51);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(112, 22);
+            this.button1.Size = new System.Drawing.Size(112, 26);
             this.button1.TabIndex = 3;
             this.button1.Text = "Enter";
             this.button1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -75,20 +63,13 @@
             // 
             // DatePicker
             // 
-            this.DatePicker.Location = new System.Drawing.Point(12, 23);
-            this.DatePicker.Name = "DatePicker";
-            this.DatePicker.Size = new System.Drawing.Size(200, 26);
-            this.DatePicker.TabIndex = 4;
-            // 
             // DayForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(64)))), ((int)(((byte)(65)))));
             this.ClientSize = new System.Drawing.Size(500, 100);
-            this.Controls.Add(this.DatePicker);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Arial", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -105,8 +86,6 @@
         #endregion
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Timer timer;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.DateTimePicker DatePicker;
     }
 }
